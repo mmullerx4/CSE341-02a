f4aee7623d74a299c06b4d4d6a84b573640458bd
mmullerx4 <mmullerx4@gmail.com>
2024-03-03 01:35:49 -0500

    w08 assign fix!!

--- swagger-ui-config.js
+++ swagger-ui-config.js
@@ -1,11 +1,12 @@
-//const swaggerUi = require('swagger-ui-express');
-const swaggerUi = require('swagger-ui-express');const swaggerDocument = require('../swagger.json');
+//const SwaggerUI = require('swagger-ui');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 
 //To pass OAuth settings to Swagger UI correctly, you can directly include the OAuth configuration when creating the Swagger UI instance
-const ui = SwaggerUI({...});
+const uiInstance = swaggerUi.setup({swaggerDocument});
 
-ui.unitOAuth({
+uiInstance.unitOAuth({
     clientId: "LDz16qaGJEjNvtGd1aMl7M6xfMg8TdOP",
     clientSecret: "B-0YddiiEbAaAhKqOPuRs8bh44xFJAByM_ORRqTkfEZmmk-3SHO40Ek82ZjY_MsR",
     realm: "https://cse341-02a.onrender.com",
@@ -19,4 +20,4 @@ ui.unitOAuth({
 
 
 
-module.exports = { ui, swaggerDocument };
+module.exports = { uiInstance, swaggerDocument };
