6427cec0b05ef672f31c1817a2d65ed235149c88
mmullerx4 <mmullerx4@gmail.com>
2024-03-03 01:47:50 -0500

    w08 fix!!!

--- swagger-ui-config.js
+++ swagger-ui-config.js
@@ -4,20 +4,23 @@ const swaggerDocument = require('./swagger.json');
 
 
 //To pass OAuth settings to Swagger UI correctly, you can directly include the OAuth configuration when creating the Swagger UI instance
-const uiInstance = swaggerUi.setup({swaggerDocument});
 
-uiInstance.unitOAuth({
-    clientId: "LDz16qaGJEjNvtGd1aMl7M6xfMg8TdOP",
-    clientSecret: "B-0YddiiEbAaAhKqOPuRs8bh44xFJAByM_ORRqTkfEZmmk-3SHO40Ek82ZjY_MsR",
-    realm: "https://cse341-02a.onrender.com",
-    appName: "Clue",
-    scopeSeparator: " ",
-    scopes: "openid profile",
-    additionalQueryStringParams: { test: "hello" },
-    useBasicAuthenticationWithAccessCodeGrant: true,
-    usePkceWithAuthorizationCodeGrant: true
-});
+const swaggerOptions = {
+    swaggerOptions: {
+        OAuth: {
+        clientId: "LDz16qaGJEjNvtGd1aMl7M6xfMg8TdOP",
+        clientSecret: "B-0YddiiEbAaAhKqOPuRs8bh44xFJAByM_ORRqTkfEZmmk-3SHO40Ek82ZjY_MsR",
+        realm: "https://cse341-02a.onrender.com",
+        appName: "Clue",
+        scopeSeparator: " ",
+        scopes: "openid profile",
+        additionalQueryStringParams: { test: "hello" },
+        useBasicAuthenticationWithAccessCodeGrant: true,
+        usePkceWithAuthorizationCodeGrant: true
+    }
+  }
+};
 
-
+const uiInstance = swaggerUi.setup(swaggerDocument, swaggerOptions);
 
 module.exports = { uiInstance, swaggerDocument };
