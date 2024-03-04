3c76c3746ac2781d6655587509e62ff954b92be0
mmullerx4 <mmullerx4@gmail.com>
2024-03-03 21:01:20 -0500

    w08 assign fix

--- routes/characters.js
+++ routes/characters.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const router = express.Router();
-//const express = require('express');
 const charactersController = require('../controllers/characters');
 const validation = require('../middleware/validate');
 const { requiresAuth } = require('express-openid-connect');
+
+router.use(cors());
 
 router.get('/', charactersController.getAll);
  //router.get('/', professionalController.getData);
--- routes/weapons.js
+++ routes/weapons.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const router = express.Router();
 //const express = require('express');
 const weaponsController = require('../controllers/weapons');
 const validation = require('../middleware/validate');
 const { requiresAuth } = require('express-openid-connect');
+
+router.use(cors());
 
 router.get('/', weaponsController.getAll);
  //router.get('/', professionalController.getData);
